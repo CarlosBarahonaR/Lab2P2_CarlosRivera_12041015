8a09fa5c17e9f802d47bfecb64b9e1618a5622e5
CJBarahona <cbarahona@t-developing.com>
2022-04-29 13:39:28 -0600

    Opcion 1

--- Lab2P2_CarlosBarahona_12041015/src/lab2p2_carlosbarahona_12041015/Lab2P2_CarlosBarahona_12041015.java
+++ Lab2P2_CarlosBarahona_12041015/src/lab2p2_carlosbarahona_12041015/Lab2P2_CarlosBarahona_12041015.java
@@ -20,10 +20,39 @@ public class Lab2P2_CarlosBarahona_12041015 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        
+
         Scanner s = new Scanner(System.in);
         Random r = new Random();
         ArrayList<Jugadores> jugadores = new ArrayList();
+        int opcion = 11;
+        while (opcion != 0) {
+            System.out.println("Bienvenido\n"
+                    + "¿Qué desea hacer?\n"
+                    + "0) Salir\n"
+                    + "1) Crear Jugador\n"
+                    + "2) Listar Jugador Por Posición\n"
+                    + "3) Listar Jugadores\n"
+                    + "4) Jugar");
+            opcion = s.nextInt();
+            switch (opcion) {
+                case 1: {
+                    System.out.println("Ingrese el nombre del usuario");
+                    String nombre=s.next();
+                  for (int x = 0; x < jugadores.size(); x++) {
+                        String verificicarN = "";
+
+                        if (jugadores.get(x) instanceof Jugadores) {
+                            verificicarN = ((Jugadores) jugadores.get(x)).getUsuario();
+                        }
+
+                        while (nombre.equals(verificicarN)) {
+                            System.out.println("Ingrese un nombre de usuario que no este tomado");
+                            nombre = s.next();
+                        }
+                    }
+                }
+            }
+        }
     }
-    
+
 }
